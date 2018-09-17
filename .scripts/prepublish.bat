f71c523a861abdf5b778f8160b2264e325271537
Aleksey Glukharev <neogarrigus@gmail.com>
2018-09-17 18:25:06 -0400

    1.0.12
    Added prepublish.bat script

---
+++ .scripts/prepublish.bat
@@ -0,0 +1,8 @@
+@echo off
+echo "=> Transpiling 'src' into ES5 ..."
+echo ""
+rd /s /q ./dist
+set NODE_ENV=production
+./node_modules/.bin/babel.cmd --ignore tests,stories --plugins "transform-runtime" ./src --out-dir ./dist
+echo ""
+echo "=> Transpiling completed."
