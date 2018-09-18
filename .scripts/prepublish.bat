57c362355514bbb7158c202587bcf26320197cb0
Aleksey Glukharev <neogarrigus@gmail.com>
2018-09-17 20:44:50 -0400

    Updated mocha package to 5.2.0

--- .scripts/prepublish.bat
+++ .scripts/prepublish.bat
@@ -1,7 +1,7 @@
 @echo off
 echo "=> Transpiling 'src' into ES5 ..."
 echo ""
-rd /s /q ./dist
+rd /s /q dist
 set NODE_ENV=production
 ./node_modules/.bin/babel.cmd --ignore tests,stories --plugins "transform-runtime" ./src --out-dir ./dist
 echo ""
